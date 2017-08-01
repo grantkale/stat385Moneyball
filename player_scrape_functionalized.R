a7ba2973a443f356b8c841bbcd1c0d36cdb52d26
Grant Kale <grkale2@illinois.edu>
2017-07-31 23:20:32 -0500

    fixed merge problem

--- player_scrape_functionalized.R
+++ player_scrape_functionalized.R
@@ -32,69 +32,6 @@ clean_data_frame = function(x){
   x[,1] = gsub("(Jul )", "2016-7-", x[,1])
   x[,1] = gsub("(Aug )", "2016-8-", x[,1])
   x[,1] = gsub("(Sep )", "2016-9-", x[,1])
-  opp = x$OPP
-  opp = gsub("(@ )", "", opp)
-  opp = gsub("vs .*", "HOME", opp)
-  oppid = 1:length(opp)
-  for(i in 1:length(opp)){
-    oppid[i] = switch(opp[i],
-                      "HOME" = "KORD",
-                      "ARI" = "KPHX",
-                      "CIN" = "KLUK",
-                      "STL" = "KSTL",
-                      "PIT" = "KPIT",
-                      "MIL" = "KMKE",
-                      "SF" = "KSFO",
-                      "PHI" = "KPHL",
-                      "ATL" = "KATL",
-                      "WSH" = "KDCA",
-                      "CHW" = "KORD",
-                      "MIA" = "KMIA",
-                      "LAD" = "KSLI",
-                      "SD" = "KSAN",
-                      "HOU" = "HOU",
-                      "COL" = "KDEN",
-                      "OAK" = "KOAK",
-                      "LAA" = "KSNA",
-                      "NYM" = "KLGA")
-  }
-  x$OPPID = oppid
-  return(x)
-}
-
-
-
-
-#Cleaning Function
-clean_data_frame = function(x){
-  colnames(x) <- x[2,]
-  x = x[-c(1,2),]
-  x = x[-c(171:177), ]
-  x = x[!(x$DATE == "Monthly Totals"), ]
-  x = x[!(x$DATE == "DATE"), ]
-  x[ x == "Did not play" ] = NA
-  colnames(x)[7] = "B2"
-  colnames(x)[8] = "B3"
-  x = transform(x, AB = as.numeric(AB),
-                R = as.numeric(R),
-                H = as.numeric(H),
-                B2 = as.numeric(B2),
-                B3 = as.numeric(B3),
-                HR = as.numeric(HR),
-                RBI = as.numeric(RBI),
-                BB = as.numeric(BB),
-                SO = as.numeric(SO),
-                SB = as.numeric(SB),
-                CS = as.numeric(CS))
-  x = x[,-c(3)]
-  x = x[,-c(14:17)]
-  #Changes the date into a usable form for the weatherData package
-  x[,1] = gsub("(Apr )", "2016-4-", x[,1])
-  x[,1] = gsub("(May )", "2016-5-", x[,1])
-  x[,1] = gsub("(Jun )", "2016-6-", x[,1])
-  x[,1] = gsub("(Jul )", "2016-7-", x[,1])
-  x[,1] = gsub("(Aug )", "2016-8-", x[,1])
-  x[,1] = gsub("(Sep )", "2016-9-", x[,1])
   
   opp = x$OPP
   opp = gsub("(@ )", "", opp)
